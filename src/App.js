10be2fbdd202b9d3fffc5528ac26c1c6d26e7369
Ramit <ramit.seth.developer@gmail.com>
2021-02-20 14:18:04 +0530

    updated-folder-structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import CharacterCounter from './components/CharacterCounter';
-import Counter from './components/Counter';
-import PasswordMatch from './components/PasswordMatch';
-import AlphaNumericPassword from './components/AlphaNumericPassword';
-import AddToCart from './components/AddToCart';
-import ToggleColor from './components/ToggleColor';
-import FigmaTool from './components/FigmaTool';
+import CharacterCounter from './components/twittercounter/CharacterCounter';
+import Counter from './components/counter/Counter';
+import PasswordMatch from './components/passwordmatch/PasswordMatch';
+import AlphaNumericPassword from './components/alphanumericpassword/AlphaNumericPassword';
+import AddToCart from './components/cart/AddToCart';
+import ToggleColor from './components/togglecolor/ToggleColor';
+import FigmaTool from './components/minifigma/FigmaTool';
 import Tabs from './components/tabs/Tabs';
 
 
